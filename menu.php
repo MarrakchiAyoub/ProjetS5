98ab80d71932b61d910651d7ddbd472c3487f449
Ayoub.Marrakchi <Marrakchi.Ay@gmail.com>
2017-12-30 12:58:39 +0000

    update ayoub 2

---
+++ menu.php
@@ -0,0 +1,44 @@
+<?php
+$cycle="Cycle d'Ingenieur";
+echo '<div class="c8">
+<nav id="topNav">
+<ul id="responsivemenu">
+    <li class="active"><a href="index.html"><i class="icon-home homeicon"></i><span class="showmobile">Home</span></a></li>
+    <li><a href="#">DEPARTEMENT</a>
+      <ul style="display: none;">
+      <li><a href="about.html">A propos</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="faq.html">F.A.Q.s</a></li>					
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="timeline.html">Timeline</a></li>
+        <li class="last"><a href="404.html">404 Page</a></li>
+    </ul>
+    </li>
+    <li><a href="#">Elements</a>
+    <ul style="display: none;">					
+        <li><a href="team.html">Team</a></li>
+        <li><a href="pricing.html">Pricing Tables</a></li>
+        <li><a href="columns.html">Columns</a></li>
+        <li><a href="rightsidebar.html">Right Sidebar</a></li>
+        <li><a href="leftsidebar.html">Left Sidebar</a></li>					
+        
+    </ul>
+    </li>
+    <li><a href="#">Emplois du temps</a>
+      <ul>
+        <li><a href="emploi-LGI.html">Licence</a></li>
+        <li><a href="portfolio-masonry4.html">Master</a></li>
+        <li><a href="portfolio-masonry5.html">'.$cycle.'</a></li>					
+        <li><a href="singleproject.html">Doctorat</a></li>
+    </ul>
+    </li>
+    <li class="last"><a href="#">Se Connecter</a>
+        <ul>
+        <li><a href="connectetu.php">Etudiant</a></li>
+        <li><a href="connectpro.php">Professeur</a></li>
+        </ul>
+    </li>
+</ul>
+</nav>
+</div>';
+?>
