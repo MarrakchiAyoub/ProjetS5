c252309905f0919d4411a767df98410dee82a17f
noughel <nourelhouda.el@gmail.com>
2018-01-13 15:40:54 +0000

    master1

--- master1.php
+++ master1.php
@@ -33,13 +33,14 @@
 <!-- HEADER
 ================================================== -->
 <div class="undermenuarea">
-	<div class="boxedshadow"></div>
+	<div class="boxedshadow">
+	</div>
 	<div class="grid">
 		<div class="row">
 			<div class="c8">
-				<h1 class="titlehead">Licence Genie informatique</h1>
+				<h1 class="titlehead">Master Systemes Informatiques et Reseaux</h1>
 			</div>
-			<div class="c4">				
+			<div class="c4">
                 <h1 class="titlehead rightareaheader"><?php if(isset($_SESSION['nom'])) echo '<i class="icon-user"></i>'; echo " ".$nom." ".$pre ?></h1>
             </div>
 		</div>
@@ -55,48 +56,34 @@
 				<span>Objectifs de la formation:</span>
 				</h1>
 				<p>
-                Le Master Systèmes Informatiques et Réseaux répond a besoin de formation par la recherchedans le domaine de technologies de l’information et de la communication caractérisée par laconvergence des techniques des réseaux et des applications informatiques.
+                <span class="dropcap">L</span>e Master Systèmes Informatiques et Réseaux répond a besoin de formation par la recherchedans le domaine de technologies de l’information et de la communication caractérisée par laconvergence des techniques des réseaux et des applications informatiques.
 
                 La création de ceMaster résulte d’un constat intégrant non seulement des considérations scientifiques ettechniques mais également les aspects économiques et industriels liés aux avancées dans lesdomaines des Technologies de l’information et de la Communication.
 				</p>
-			</div>
-            <div class="c8">
 				<h1 class="maintitle ">
 				<span>Public cible et conditions d'admission:</span>
 				</h1>
-				
 					L'accès aux formations de Master en Sciences et techniques est ouvert aux titulaires :
-                    <ul  style="list-style-type:disc">
-                        <li>De la licence dans le domaine de formation du Master en Sciences et Techniques ou d’undiplôme reconnu équivalent satisfaisant aux critères d’admission prévus dans le descriptif dela filière.. </li>
-                        <li>De diplômes au moins d’un niveau équivalent de la licence, sur étude de dossier et/ou parvoie de concours, et satisfaisant aux critères d’admission prévus dans le descriptif de la filière.</li>
-                        <li>Pré-requis pédagogiques : Ecologie, Biologie, Géologie, Chimie, Statistique, Informatique. Procédures de sélection et d’entretien : sur dossier et entretien. </li>
+                    <ul  style="list-style-type:square">
+                        <li> De la licence dans le domaine de formation du Master en Sciences et Techniques ou d’undiplôme reconnu équivalent satisfaisant aux critères d’admission prévus dans le descriptif dela filière.. </li>
+                        <li> De diplômes au moins d’un niveau équivalent de la licence, sur étude de dossier et/ou parvoie de concours, et satisfaisant aux critères d’admission prévus dans le descriptif de la filière.</li>
+                        <li> Pré-requis pédagogiques : Ecologie, Biologie, Géologie, Chimie, Statistique, Informatique. Procédures de sélection et d’entretien : sur dossier et entretien. </li>
                     </ul>
-            
-			</div>
-            <div class="c8">
 				<h1 class="maintitle ">
 				<span>Competences visees et debouches:</span>
 				</h1>
 				<p>
 					<span class="dropcap">A</span>près l’obtention du Master Recherche, une alternative est offerte, soit entreprendre une formation de haut niveau par la préparation d’une thèse en informatique, soit rejoindre le mondeindustriel ou cette première expérience en recherche pourra déjà ouvrir les portes des servicesde recherche et développement. Néanmoins, la poursuite en Thèse reste la finalité première dece Master Recherche.
 				</p>
-			</div>
-    </div>
-</div>
-		<!-- Contenu S1 / S2 / S3 / S4
+                		<!-- Contenu S1 / S2 / S3 / S4
 		================================================== -->
-    
-<div class="grid">
-	=<div class="shadowundertop"></div>
-		<div class="row">
-			<!-- box 1 -->
 			<div class="c4">
-				<h1 class="faqstitle"><span class="icon-tags faqsicon"></span>CONTENU</h1>
+				<h2 class="title stresstitle">CONTENU</h2>
+					<hr class="hrtitle">
 				<dl class="faqs">
-            
 				<dt>Semestre 1</dt>
 					<dd>
-                        <ul>
+                        <ul style="list-style-type:square">
                             <li>Bases de données avancées</li>
                             <li>Théorie des graphes et applications</li>
                             <li>Architecture des réseaux et protocoles</li>
@@ -107,7 +94,7 @@
                     </dd>
                 <dt>Semestre 2</dt>
 					<dd>
-                        <ul>
+                        <ul style="list-style-type:square">
                             <li>Programmation Web et mobiles</li>
                             <li>Réseaux sans fil et mobiles</li>
                             <li>Développement d'applications distribuées</li>
@@ -118,7 +105,7 @@
                     </dd>
                 <dt>Semestre 3</dt>
 					<dd>
-                        <ul>
+                        <ul style="list-style-type:square">
                             <li>Architectures et Technologies de virtualisation</li>
                             <li>Développement d’applications mobiles</li>
                             <li>RIA et Web sémantique</li>
@@ -129,13 +116,35 @@
                     </dd>
                     <dt>Semestre 4</dt>
 					<dd>
-                        <ul>
+                        <ul style="list-style-type:square">
                             <li>PFE</li>
                         </ul>
                     </dd>
-            </dl>
+                </dl>
+            </div>
+			</div>
+            <!-- SIDEBAR -->
+            <div class="c3">
+				<div class="rightsidebar">
+					<h2 class="title stresstitle">S.I.R.</h2>
+					<hr class="hrtitle">
+					<img src="images/sir.jpg" class="imgOpa teamimage" alt="">
+					<div class="teamdescription">
+						<h1>Prof. BOUHORMA Mohammed </h1>
+						<span class="hirefor">RESPONSABLE DE FILIERE</span>
+					</div>
+					<br/>
+					<img src="images/Prof.png" class="imgOpa teamimage" alt="">
+					<div class="teamdescription">
+						<h1>Prof. BOUHORMA Mohammed </h1>
+						<span class="hirefor">RESPONSABLE DE FILIERE</span>
+					</div>
+				</div>
+			</div>       
+            <!-- end sidebar -->
+        
+
         </div>
-    </div>
 </div>
 <!-- FOOTER
 ================================================== -->
